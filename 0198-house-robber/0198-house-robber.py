33fa198618084e6b9214fa1d6e8673c16b04b834
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2023-07-11 22:59:57 +0900

    Time: 47 ms (62.96%), Space: 16.3 MB (76.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        rob1, rob2 = 0, 0
         
-        if len(nums) == 1:
-            return nums[0]
-        
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
-        
-        dp = [nums[0], nums[1], nums[0]+nums[2]]
+        for n in nums:
+            temp = max(n+rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
             
-        for i in range(3, len(nums)):
-            dp.append(nums[i] + max(dp[i-2], dp[i-3]))
-            
-        return max(dp[-1], dp[-2])
-        
+        return rob2
