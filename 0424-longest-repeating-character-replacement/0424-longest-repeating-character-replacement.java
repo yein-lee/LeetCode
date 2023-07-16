540a959c3cadc2af038bd261c11509ec322a2443
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2023-07-16 19:58:22 +0900

    Time: 31 ms (26.04%), Space: 43.4 MB (30.78%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,21 +1,21 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         HashMap<Character, Integer> count = new HashMap<>();
-        int res = 0;
-
+        int maxCount = 0, maxLength = 0;
         int l = 0;
+
         for(int r = 0; r < s.length(); r++) {
             char currentChar = s.charAt(r);
             count.put(currentChar, count.getOrDefault(currentChar, 0) + 1);
+            maxCount = Math.max(maxCount, count.get(currentChar));
 
-            if ((r - l + 1) - Collections.max(count.values()) > k) {
+            if ((r - l + 1) - maxCount > k) {
                 count.put(s.charAt(l), count.get(s.charAt(l)) - 1);
                 l++;
+            } else {
+                maxLength = Math.max(maxLength, r - l + 1);
             }
-
-            res = Math.max(res, r - l + 1);
         }
-
-        return res;
+        return maxLength;
     }
 }
