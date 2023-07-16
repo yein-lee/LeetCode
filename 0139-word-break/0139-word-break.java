0a5a33d5f822ce2d78e508e2c10b10b7eeeb9f27
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2023-07-16 17:10:42 +0900

    Time: 13 ms (9.97%), Space: 42.9 MB (65.46%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        int [] dp = new int[s.length()+1];
-        dp[s.length()] = 1;
+        boolean [] dp = new boolean[s.length()+1];
+        dp[s.length()] = true;
         
-        for(int i=s.length()-1; i>=0; i--){
-            for(int j=i+1; j<=s.length(); j++){
-                if(dp[j]>0 && wordDict.contains(s.substring(i, j))){
-                    dp[i] = 1;
+        for(int i = s.length() - 1; i>=0; i--){
+            for(int j = i + 1; j <= s.length(); j++){
+                if(dp[j] == true && wordDict.contains(s.substring(i, j))){
+                    dp[i] = true;
                 }
             }
         }
         
-        return dp[0] == 1;
+        return dp[0];
     }
 }
